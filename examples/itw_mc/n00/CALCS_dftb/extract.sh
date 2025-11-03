b6f31e410bc22185a6e6e0794d19f5df0daaab28
SRG Balestra <salrodgom@gmail.com>
2025-11-03 09:54:52 +0100

    clean repo

--- examples/itw_mc/n00/CALCS_dftb/extract.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in *.vasp.log ; do
- e=$(grep 'Total Energy:' $file | awk '{print $5}' | tail -n1)
- echo "$e # $file"
-done
--- examples/itw_mc/n02/CALCS_dftb/extract.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for f in c*.vasp.log ; do x=$(grep 'Total Energy:' $f | tail -n1| awk '{print $5}'); echo "$x # $f" ; done > ENERGIES
